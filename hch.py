385f60c0a7d6f5efe1d0252db3f3573ca3589c1f
houchuanhao <houchuanhao@imudges.com>
2016-09-07 23:08:58 +0800

    孙琴挥

--- hch.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-from xl_tool import xk,tk,tk2xk
-import time, datetime
-kcs = [
-    {'kch': '140120020', 'kxh': '01'}
-]
-print("helloworld");
-
-tk2xk("", "", "0141124825", "159753123qwe", kcs)
